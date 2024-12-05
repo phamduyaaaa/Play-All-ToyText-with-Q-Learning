4f4b648c8db41cfe6186702c7d3072e030bbbcef
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2024-12-05 18:45:51 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,22 @@
 import gymnasium as gym
 import numpy as np
+import yaml
+from utils import *
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+    NUM_EPISODES = config['epochs']
+    ALPHA = config['alpha']
+    GAMMA = config['gamma']
+    MODE = config['mode']
+    MAX_STEPS = config['max_steps']
+    EPSILON = config['epsilon']
+    ENV = config['env']
 
 def q_learning():
-    env = gym.make(ENV,is_slippery=False, render_mode="human")
-    for epoch in range(EPOCHS):
+    env = gym.make(ENV,is_slippery=False, render_mode=MODE)
+    Q_table = np.zeros((env.observation_space.n, env.action_space.n))
+    for episode in range(NUM_EPISODES):
         state, _ = env.reset()
         total_reward = 0
         steps = 0
